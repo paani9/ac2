b1474283ede9a0bd417ff1887e942cc4c5c747eb
paani9 <tpaani9@gmail.com>
2022-10-23 00:32:42 +0530

    commitas2

---
+++ src/Asp2/Asp3.java
@@ -0,0 +1,19 @@
+package Asp2;
+
+
+import asp.Asp1;
+
+public class Asp3 extends Asp1
+{public static void main(String[] args)
+{
+Asp3 a1=new Asp3();
+//a1s.car();
+a1.bike();
+a1.cycle();
+}
+
+
+
+}
+
+
